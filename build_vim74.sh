1215e294e5d337d3f8bd6211cdf85cca56f3943f
MikimotoH <mikimotoh@gmail.com>
2013-10-19 20:04:34 +0800

    use make -j 8 to accelerate build_ycm

--- build_vim74.sh
+++ build_vim74.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 (cd ~ \
     && wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 \
-    && tar zxf vim-7.4.tar.bz2 \
+    && tar zxf vim-7.4.tar.bz2)
+(cd ~/vim74 \
     && ./configure --with-features=huge \
             --enable-rubyinterp \
             --enable-pythoninterp \
--- build_ycm.sh
+++ build_ycm.sh
@@ -15,7 +15,7 @@ export C_INCLUDE_PATH=':/usr/include'
 (mkdir -p ~/ycm_build \
     && cd ~/ycm_build \
     && cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=~/ycm_temp/llvm_root_dir . ~/.vim/bundle/YouCompleteMe/cpp \
-    && make ycm_core VERBOSE=1 )
+    && make -j `sysctl -n kern.smp.cpus` ycm_core)
 
 mv ~/ycm_temp/llvm_root_dir/lib/libclang.so /usr/local/lib/
 
